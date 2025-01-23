ff3b2623bfc972fe00771ec7195ea6526e7ca662
Omkark111 <omkar.k@bayrocklabs.com>
2025-01-23 22:42:59 +0530

    add new workflow for secret

--- dnszone.tf
+++ dnszone.tf
@@ -1,4 +1,4 @@
-variable "dns_zone_name" {
+/*variable "dns_zone_name" {
   description = "The name of the DNS zone"
   type        = string
 }
@@ -32,3 +32,4 @@ resource "azurerm_dns_txt_record" "asuid" {
 
   depends_on = [azurerm_dns_zone.main]
 }
+*/
